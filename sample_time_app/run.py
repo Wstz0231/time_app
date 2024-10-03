fdf607a72192777d09c45203e705d716a8ffd4d3
WZ <midas0231@gmail.com>
2024-10-03 03:53:35 -0400

    Update code

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import pytz
+from datetime import datetime
 app = Flask(__name__)
 
 
@@ -6,6 +8,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello world!'
 
+@app.route('/time')
+def get_time():
+    tz = pytz.timezone('US/Eastern')
+    current_time = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S %Z")
+    return "Current time: " + current_time
 
 app.run(host='0.0.0.0',
         port=8080,
